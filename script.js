16cdd6ff93222ac680f38868aba6a2956c2c1807
poloiooo <98511918+poloiooo@users.noreply.github.com>
2022-04-28 08:20:42 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function CalculateSum(){
+    let firstNumber = document.getElementById("FirstNumber").value;
+    let secondNumber = document.getElementById("SecondNumber").value;
+    let result = Number(firstNumber) + Number(secondNumber);
+    alert("Aprēķinu rezultāts ir " + result);
+}
